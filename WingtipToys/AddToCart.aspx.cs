fae929a26e3ca0aa5bb8fa02e841f70617db6165
joshuafolivi <joshua.folivi@gmail.com>
2020-10-26 16:55:59 +0100

    Added functionality for adding product to cart

---
+++ WingtipToys/AddToCart.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WingtipToys.Logic;
+
+namespace WingtipToys
+{
+    public partial class AddToCart : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string rawId = Request.QueryString["ProductID"];
+            int productId;
+
+            if (!String.IsNullOrEmpty(rawId) && int.TryParse(rawId, out productId))
+            {
+                using (ShoppingCartAction shoppingCart = new ShoppingCartAction())
+                {
+                    shoppingCart.AddToCart(Convert.ToInt16(productId));
+                }
+            }
+            else
+            {
+                Debug.Fail("ERROR : We should never get to AddToCart.aspx without a ProductId.");
+                throw new Exception("ERROR : It is illegal to load AddToCart.aspx without setting a ProductId.");
+            }
+
+            Response.Redirect("ShoppingCart.aspx");
+        }
+    }
+}
